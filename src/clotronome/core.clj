c1929143877f0f9f945ad3e6ab0abcb7fa9c0c50
Lenko <lenko-d@github>
2015-01-25 23:38:42 -0600

    Changed namespace statement

--- src/clotronome/core.clj
+++ src/clotronome/core.clj
@@ -1,13 +1,13 @@
-(ns clotronome.core)
-(require '[clodiuno.core :as cld])
-(require '[clodiuno.firmata :as cldf])
+(ns clotronome.core
+  (:require [clodiuno.core :as cld])
+  (:require [clodiuno.firmata :as cldf]))
 
 (defn led-pins []
   (range 2 14))
 
 (def tempo-changer-pin 2)
 
-(def init-delay 5000)
+(def arduino-init-delay 5000)
 
 (defn blink [board led]
   (cld/digital-write board led cld/HIGH)
@@ -17,15 +17,16 @@
 
 (defn -main [& args]
   (let [board (cld/arduino :firmata "/dev/ttyUSB0")] 
-    (Thread/sleep init-delay)  ;; arduino needs some time to boot
+    (Thread/sleep arduino-init-delay)  ;; arduino needs some time to boot
 
     (doseq [pin (led-pins)]
       (cld/pin-mode board pin cld/OUTPUT))
 
     (cld/enable-pin board :analog tempo-changer-pin)
-    (Thread/sleep init-delay)
+    (Thread/sleep arduino-init-delay)
 
     (while true
       (doseq [led (led-pins)]  (blink board led))
       (doseq [led (reverse (led-pins))]  (blink board led)))))
+
 
